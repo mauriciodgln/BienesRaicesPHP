2b4b60fbdbc661f16ee42c0357e0eafdfa866f0f
mauriciodgln <67520732+mauriciodgln@users.noreply.github.com>
2022-04-04 18:53:12 -0500

    Update database.php

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 
 
 function conectarDB() : mysqli {
-  $db = mysqli_connect('localhost', 'root', 'root', 'bienes_raices');
+  $db = mysqli_connect('localhost', '', '', 'bienes_raices');
 
   if(!$db) {
     echo 'No se pudo conectar a la base de datos';
